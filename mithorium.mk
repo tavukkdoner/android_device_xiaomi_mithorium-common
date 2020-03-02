2f9dd644d22e2475fade4b4d21fb952b2308e475
Nicholas Ambur <nambur@google.com>
2020-03-02 15:05:49 -0800

    mithorium-common: Add SoundTrigger HAL v2.3 support
    Bug: 139071862
    Test: gts-tradefed run gts-dev -m GtsAssistIntentTestCases
    Change-Id: I2d17aade762ceb7800d5305119c37ec0f5991f35

--- mithorium.mk
+++ mithorium.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     sound_trigger.primary.msm8937:32 \
     tinymix
 
