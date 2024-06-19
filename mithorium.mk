4f1e4a352a01f6f491ec3d17a2616009282626c1
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    mithorium-common: Build AOSP Radio HIDL->AIDL Wrapper
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- mithorium.mk
+++ mithorium.mk
@@ -561,6 +561,7 @@ PRODUCT_PACKAGES += \
     librmnetctl
 
 PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat \
     android.hardware.radio.c_shim@1.0 \
     android.hardware.radio.c_shim@1.1 \
     android.hardware.radio.c_shim@1.2 \
