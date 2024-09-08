8a3848305503cd63e753d253d548c183bbc2fe38
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-09-08 19:05:14 +0300

    mithorium-common: Create powerhint.json
    * Based on sunfish
    * https://cs.android.com/android/platform/superproject/main/+/main:device/google/sunfish/powerhint.json
    * https://cs.android.com/android/platform/superproject/main/+/main:device/google/sunfish/device.mk;l=892
    * https://cs.android.com/android/platform/superproject/+/master:frameworks/rs/perf-test-scripts/set-gpu-freq;bpv=0
    * https://cs.android.com/android/platform/superproject/main/+/main:frameworks/base/core/java/android/os/PowerManagerInternal.java;l=220

--- mithorium.mk
+++ mithorium.mk
@@ -455,6 +455,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint/system/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/powerhint/vendor/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/powerhint/vendor/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
