4d9bd4930e5607ff49b5758eb1738cb5e7c00ca3
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-04 20:09:20 +0800

    mithorium-common: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- mithorium.mk
+++ mithorium.mk
@@ -578,7 +578,7 @@ endif
 MITHORIUM_PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.basic \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 ifneq ($(TARGET_USES_DEVICE_SPECIFIC_VIBRATOR),true)
