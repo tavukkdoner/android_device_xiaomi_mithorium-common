08cd005ec86bc3ece85fd66f3953c0e111e38bf3
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-11-12 08:10:30 +0200

    Revert "mithorium-common: Ensure we build the vendor version of libcrypto_shim"
    This reverts commit 5c8130e578617b8eed818cf6469cbd1b6c353ab8.

--- mithorium.mk
+++ mithorium.mk
@@ -288,7 +288,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
 MITHORIUM_PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor \
-    libcrypto_shim.vendor
+    libcrypto_shim
 
 # Dumpstate
 MITHORIUM_PRODUCT_PACKAGES += \
