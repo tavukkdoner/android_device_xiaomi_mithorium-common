7f41d131f39c987c3b294afb7601b8528666ca68
Vladimir Mikhailov <beykerykt@gmail.com>
2025-01-01 14:34:21 +0200

    mithorium-common: Import task_profiles.json from android_system_core
    * https://github.com/LineageOS/android_system_core/blob/lineage-22.1/libprocessgroup/profiles/task_profiles_28.json
    
    Change-Id: I60e551950c96bc0d13ee6b8eee21cbb4b01e70b8

--- mithorium.mk
+++ mithorium.mk
@@ -465,7 +465,7 @@ MITHORIUM_PRODUCT_PACKAGES += \
 # Power
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint/system/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml \
