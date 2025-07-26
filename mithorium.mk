e781a9b136a1ba2c111dc91a92cc167a039898cf
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-07-26 08:17:04 +0300

    mithorium-common: bring cherish

--- mithorium.mk
+++ mithorium.mk
@@ -454,7 +454,7 @@ endif
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-cherish
 ifeq ($(TARGET_HAS_NO_RADIO),true)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-noradio
 else
@@ -577,7 +577,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/cherish/interfaces/power-libperfmgr \
     $(LOCAL_PATH)
 
 # Subsystem state notifier
