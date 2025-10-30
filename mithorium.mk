bcddc0c783e8b196946a664282e9cc75abd4cf39
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-10-30 19:58:20 +0200

    Revert "mithorium-common: Migrate to AIDL LiveDisplay HAL"
    This reverts commit 01eaacbfca88c9a4c94fb1bc4c8e07c4cb144648.

--- mithorium.mk
+++ mithorium.mk
@@ -388,9 +388,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sdm
-
-$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 ifeq ($(TARGET_BOARD_PLATFORM),msm8953)
