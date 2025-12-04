6e595fdaa5d9eadcf74145fd95fc42c55198fe93
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-04 16:48:28 +0200

    mithorium-common: Soft restart is deprecated in Android 15.
    * Ref: https://source.android.com/docs/core/runtime/soft-restart
    
    Change-Id: I6d3243e7a060965ea5b67a737869adbbc6d67c34

--- mithorium.mk
+++ mithorium.mk
@@ -6,9 +6,6 @@
 
 TARGET_USES_XIAOMI_MITHORIUM_COMMON_TREE := true
 
-# Userspace Reboot
-$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
-
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
