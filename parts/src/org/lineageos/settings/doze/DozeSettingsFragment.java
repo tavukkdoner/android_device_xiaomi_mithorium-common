64339c672f98340066504e7cb2acd634902c8b72
Bruno Martins <bgcngm@gmail.com>
2025-12-21 19:23:46 +0200

    mithorium-common: parts: Clean up imports
    Change-Id: I483a137865e0e558ecadfb1ed25d0f46daf0f3b2

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -19,9 +19,6 @@ package org.lineageos.settings.doze;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
