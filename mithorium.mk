a5fcb3df3bab362b42e3dd82fe5e7ecdff3c04a0
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2026-05-08 11:15:13 +0300

    mithorium-common: Include Sony Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- mithorium.mk
+++ mithorium.mk
@@ -171,6 +171,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
